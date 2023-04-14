4890c91af12cf3f42f4c5fda9de48cc15f79468a
186Sergey <papa19700303@mail.ru>
2023-04-15 00:03:10 +0500

    My first commit

--- adduser.py
+++ adduser.py
@@ -2,7 +2,7 @@ import tkinter as tk
 from tkinter import messagebox as mb
 from tkinter import ttk
 import sqlite3 as sq
-import re
+
     
 
 with  sq.connect("db/avtoservise.db") as conn:
@@ -13,25 +13,32 @@ with  sq.connect("db/avtoservise.db") as conn:
                 username TEXT, 
                 password TEXT)''')
     
+
 def save_users():
+    """
+    The function adds user data to the database. Also checks the password for validity.\n
+    Функция добавляет данные о пользователях в БД. Также проверяет пароль на валидность.
+    """
     surname = ent_surname_user.get()
     username = ent_name_user.get()
     passwd = ent_password_user.get()
     cur.execute('''INSERT INTO users(surname, username, password) 
                    VALUES (?, ?, ?)''', 
                    (surname, username, passwd))
-    conn.commit()
-    
-    if len(passwd) <7:
-        mb.showinfo("Сохранение", f"Данные не сохранены {username}!\nДлина пароля должа быть не менее 7 символов.") 
+          
+    if len(passwd) < 6:
+        mb.showinfo("Сохранение", f"Данные не сохранены {username}!\nДлина пароля должа\
+            быть не менее 6 символов.") 
+        return False
     else:
         mb.showinfo("Сохранение", f"Данные записаны в базу данных {username}!")
+    conn.commit()
     root.destroy()
 
 
 root = tk.Tk()
 root.title("Регистрация нового работника")
-root.geometry("450x280")
+root.geometry("450x300")
 root.resizable(False, False)
 root.iconphoto(True, tk.PhotoImage(file="logo.png"))
 
@@ -54,15 +61,20 @@ ent_name_user = ttk.Entry(root, validate="key")
 ent_name_user.grid(row=2, column=1, padx=15, pady=10)
 ent_name_user.config(font=("Times New Roman", 12), foreground="grey", width=30)
 
-lbl_password_user = ttk.Label(root, text="Пароль")
+lbl_password_user = ttk.Label(root, text="Пароль*")
 lbl_password_user.grid(row=3, column=0, padx=15, pady=10, sticky=tk.W)
 lbl_password_user.config(font=("Times New Roman", 12, "bold"))
 ent_password_user = ttk.Entry(root, show="*")
 ent_password_user.grid(row=3, column=1, padx=15, pady=10)
 ent_password_user.config(font=("Times New Roman", 12), foreground="#000000", width=30)
 
+lbl_new_user = ttk.Label(root, text="* Пароль должен состоять из строчных и заглавных букв,\
+цифр и спецсимволов")
+lbl_new_user.grid(row=4, column=0, columnspan=2, padx=35, pady=5, sticky=tk.E)
+lbl_new_user.config(font=("Times New Roman", 8, "bold"))
+
 btn_submit_db = ttk.Button(root, text="Сохранить", width=25, command=save_users)
-btn_submit_db.grid(row=4, column=1, padx=35, pady=20, sticky=tk.NE)
+btn_submit_db.grid(row=5, column=1, padx=35, pady=20, sticky=tk.NE)
 
 
 root.mainloop()
