50b887551435067d59cc09e2668aad3554a64624
Сергей <130276187+186Sergey@users.noreply.github.com>
2023-09-24 21:26:47 +0500

    Implementation of data output from the database
    Implementation of data output from the database in the TEXT field

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ from tkinter import messagebox as mb
 import sqlite3 as sql
 
 
+
 def myclose():
     """
     The function asks the user if he really wants to shut down the application\n
@@ -13,33 +14,40 @@ def myclose():
         root.destroy()
 
 
-with sql.connect("db/avtoservise.db") as requisites:
+with sql.connect("carservise.db") as requisites:
+    # Вывод названия организации
     cur = requisites.cursor()
-    cur.execute('''SELECT organisation FROM service''')
+    cur.execute('''SELECT firma FROM service''')
     x = cur.fetchone()
 
-
-
-with sql.connect("db/avtoservise.db") as client:
-    cur = client.cursor()
-    klient = ('''SELECT persone.rowid, mydata, persone.surname, persone.username, persone.patronymic, persone.city, persone.street,
-                        persone.phfone, persone.email, sts, marka_auto, model, gos_nomer, odometr, 
-                        description FROM persone JOIN users ON persone.rowid = users.id''')
-    cur.execute(klient)
+    cur.execute('''SELECT rowid, mydata, surname, username, phfone, email, marka_auto, model, gos_nomer, odometr, description FROM persone''')
     records = cur.fetchall()
     for row in records:
-        result = "Запись №: - {0}\nДата: - {1}\nФамилия: - {2}\nИмя: - {3}\nОтчество: - {4}\n\
-Город: - {5}\nУлица: - {6}\nТелефон: - {7}\nEmail: - {8}\nСТС: - {9}\nМарка автомобиля: - {10}\n\
-Модель автомобиля: - {11}\nГос. номер: - {12}\nОдометр: - {13}\nНеисправность:\n{14}\n"\
+        result_client = "Запись №: - {0}\nДата: - {1}\nФамилия: - {2}\nИмя: - {3}\nТелефон: - {4}\nEmail: - {5}\nМарка автомобиля: - {6}\n\
+Модель автомобиля: - {7}\nГос. номер: - {8}\nОдометр: - {9}\nНеисправность:\n{10}\n\n"\
+            .format(row[0], row[1], row[2], row[3], row[4], row[5], row[6], row[7], row[8], row[9], row[10])
+
+    cur.execute('''SELECT firma, region, city, street, myindex, surname, username, patronymic, phfone, fax, email FROM service''')
+    records = cur.fetchall()
+    for row in records:
+        result_firma = "Организация: - {0}\nРегион: - {1}\nГород: - {2}\nУлица: - {3}\nИндекс: - {4}\nФамилия: - {5}\n\
+Имя: - {6}\nОтчество: - {7}\nТелефон: - {8}\nФакс: - {9}\nEmail: - {10}\n\n"\
             .format(row[0], row[1], row[2], row[3], row[4], row[5], row[6], row[7], row[8], row[9],
-                    row[10], row[11], row[12], row[13], row[14])
-    # for row in records:
-    #     res = "Фамилил работника: - \n{1}\n"\
-    #         .format(row[1])
-                        
+                    row[10])
+
+    cur.execute('''SELECT surname FROM users''')
+    records = cur.fetchall()
+    for row in records:
+        result_worker = "Фамилия работника: - {0}\n\n".format(row[0])
+
+    cur.execute('''SELECT kol, payment FROM oplata''')
+    records = cur.fetchall()
+    for row in records:
+        oplacheno = "Запчасти: - {0}\nОплачено: - {1}\n\
+            =====================================================================================\n".format(row[0], row[1])
 
 
-with sql.connect("db/avtoservise.db") as bqbd:
+with sql.connect("carservise.db") as bqbd:
         cur = bqbd.cursor()
 
 def backupdb():
@@ -63,10 +71,10 @@ def restore():
 
 root = tk.Tk()
 root.protocol('WM_DELETE_WINDOW', myclose)
-root.title("Сведения о реквизитах СТО и собственнике")
-root.geometry("825x900+250+20")
+root.title("Информация об автомобиле и заказчике")
+root.geometry("825x780+250+20")
 root.resizable(False, False)
-root.iconphoto(True, tk.PhotoImage(file="service.png"))
+root.iconphoto(True, tk.PhotoImage(file="logo.png"))
 
 maim_menu = tk.Menu(root)
 root.configure(menu = maim_menu)
@@ -76,6 +84,7 @@ maim_menu.add_cascade(label = "Файл", menu=first_column)
 first_column.add_command(label="Добавить реквизиты")
 first_column.add_command(label="Добавить работника")
 first_column.add_command(label="Добавить запись")
+first_column.add_command(label="Поиск записи")
 first_column.add_separator()
 first_column.add_command(label="Выход", command=myclose)
 
@@ -87,7 +96,7 @@ second_column.add_command(label="Редактировать запись")
 third_column = tk.Menu(maim_menu, tearoff=0)
 maim_menu.add_cascade(label = "Сервис", menu=third_column)
 third_column.add_command(label="Восстановить пароль")
-third_column.add_command(label="Бэкап БД", command=backupdb)
+third_column.add_command(label="Резервная копия БД", command=backupdb)
 third_column.add_command(label="Восстановить БД", command=restore)
 
 fourth_column = tk.Menu(maim_menu, tearoff=0)
@@ -101,9 +110,13 @@ main_frame.grid(row=0, column=0, columnspan=5)
 main_text_lbl = tk.Label(main_frame, text=x[0].upper(), font=("Times New Roman", 20, "bold"), fg="green")
 main_text_lbl.grid(row=1, column=0, columnspan=5, padx=20, pady=20)
 
-main_text = tk.Text(main_frame, width=95, height=30, wrap=tk.WORD)
-main_text.grid(row=2, column=0, padx=15, pady=5)
-main_text.insert(tk.END, result)
+main_text = tk.Text(main_frame, width=96, height=35, wrap=tk.WORD)
+main_text.grid(row=2, column=0, padx=10, pady=5)
+main_text.insert(tk.END, result_client)
+main_text.insert(tk.END, result_firma)
+main_text.insert(tk.END, result_worker)
+main_text.insert(tk.END, oplacheno)
+
 
 
 
@@ -111,9 +124,8 @@ scroll = tk.Scrollbar(main_frame, orient="vertical", command=main_text.yview)
 scroll.grid(row=2, column=0, sticky="ens")
 main_text.config(font=("Times New Roman", 12), padx=15, pady=10, yscrollcommand=scroll.set)
 
-statusBar = tk.Label(root, relief="sunken", anchor="w", fg="green", font=("Times New Roman", 14))
-statusBar.grid(row=10, column=0, columnspan=5, sticky="w", padx=10)
+# statusBar = tk.Label(root, relief="sunken", anchor="w", fg="green", font=("Times New Roman", 14))
+# statusBar.grid(row=10, column=0, columnspan=5, sticky="w", padx=10)
 
-
-            
+               
 root.mainloop()
