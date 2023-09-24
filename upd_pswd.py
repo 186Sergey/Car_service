cf99b9b99904f875a31c3f5ee07a908ba1bb1ae5
Сергей <130276187+186Sergey@users.noreply.github.com>
2023-09-24 21:37:40 +0500

    The function updates the password
    The password update function has been changed

--- upd_pswd.py
+++ upd_pswd.py
@@ -2,7 +2,6 @@ import tkinter as tk
 from tkinter import messagebox as mb
 from tkinter import ttk
 import sqlite3 as sq
-import main
 
 
 
@@ -11,18 +10,19 @@ def forgot():
     The function updates the password if the user has forgotten it.\n
     Функция обновляет пароль, если пользователь забыл его.
     """
-    with  sq.connect("db/avtoservise.db") as conn:
+    with  sq.connect("carservise.db") as conn:
         cur = conn.cursor()
         surname = ent_surname_user.get()
         passwd = ent_password_user.get()
-        cur.execute("""UPDATE users SET password = ? WHERE surname = ?""", (passwd, surname))
         
-        if len(passwd) < 8:
-            mb.showinfo("Восстановление", "Длина пароля должна быть не менее 8 символов.") 
+        if len(passwd) < 5:
+            mb.showinfo("Восстановление", "Длина пароля должна быть не менее 5 символов.") 
             return False
         else:
+            cur.execute("""UPDATE users SET password = ? WHERE surname = ?""", (passwd, surname))
             mb.showinfo("Восстановление", "Пароль изменён!")
-        conn.commit()    
+        conn.commit()
+        conn.close()    
         root.destroy()
 
 
@@ -34,36 +34,25 @@ root.iconphoto(True, tk.PhotoImage(file="logo.png"))
 
 lbl_new_user = ttk.Label(root, text="Восстановление пароля")
 lbl_new_user.grid(row=0, column=0, columnspan=2, padx=55, pady=15)
-lbl_new_user.config(font=("Times New Roman", 18, "bold"), foreground="red")
-
-def validate(input):
-    """
-    The function checks the Entry field for characters entered by the user from the keyboard,\
-        letters must be entered.\n
-    Функция проверяет поле Entry на вводимые символы пользователем с клавиатуры,\
-        должны быть введены буквы.
-    """
-    return input.isalpha()
-valid = root.register(validate)
+lbl_new_user.config(font=("Times New Roman", 18), foreground="red")
 
 lbl_surname_user = ttk.Label(root, text="Фамилия")
 lbl_surname_user.grid(row=1, column=0, padx=15, pady=10, sticky=tk.W)
-lbl_surname_user.config(font=("Times New Roman", 12, "bold"))
-ent_surname_user = ttk.Entry(root, validate="key",validatecommand=(valid,"%S"))
+lbl_surname_user.config(font=("Times New Roman", 12))
+ent_surname_user = ttk.Entry(root)
 ent_surname_user.grid(row=1, column=1, padx=15, pady=10)
 ent_surname_user.config(font=("Times New Roman", 12), foreground="grey", width=30)
 
 lbl_password_user = ttk.Label(root, text="Новый пароль")
 lbl_password_user.grid(row=2, column=0, padx=15, pady=10, sticky=tk.W)
-lbl_password_user.config(font=("Times New Roman", 12, "bold"))
+lbl_password_user.config(font=("Times New Roman", 12))
 ent_password_user = ttk.Entry(root, show="*")
 ent_password_user.grid(row=2, column=1, padx=15, pady=10)
 ent_password_user.config(font=("Times New Roman", 12), foreground="#000000", width=30)
 
-lbl_new_user = ttk.Label(root, text="* Пароль должен состоять из строчных и заглавных букв,\
- цифр и спецсимволов")
-lbl_new_user.grid(row=4, column=0, columnspan=2, padx=35, pady=5, sticky=tk.E)
-lbl_new_user.config(font=("Times New Roman", 8, "bold"))
+lbl_new_user = ttk.Label(root, text="* Пароль должен состоять из строчных и заглавных букв, цифр и спецсимволов", foreground="blue")
+lbl_new_user.grid(row=4, column=0, columnspan=2, padx=35, pady=5, sticky=tk.W)
+lbl_new_user.config(font=("Times New Roman", 9, "italic"))
 
 btn_submit_db = ttk.Button(root, text="Обновить", width=25, command=forgot)
 btn_submit_db.grid(row=5, column=1, padx=35, pady=20, sticky=tk.NE)
