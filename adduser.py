75916da1c36602cdf175c09e779cb41a37818f4e
Сергей <130276187+186Sergey@users.noreply.github.com>
2023-09-24 21:15:17 +0500

    Add files via upload
    Fixed adding a user to the database with an incorrectly typed password

--- adduser.py
+++ adduser.py
@@ -6,7 +6,7 @@ import sqlite3 as sq
 
     
 
-with  sq.connect("db/avtoservise.db") as conn:
+with  sq.connect("carservise.db") as conn:
     cur = conn.cursor()
     cur.execute('''CREATE TABLE IF NOT EXISTS users
                 (id INTEGER PRIMARY KEY, 
@@ -17,24 +17,24 @@ with  sq.connect("db/avtoservise.db") as conn:
 
 def save_users():
     """
-    The function adds user data to the database. Checks the password for complexity (number\
-        input characters.)\n
-    Функция добавляет данные о пользователях в БД. Проверяет пароль на сложность (количество\
-        вводимых символов.)
+    The function adds user data to the database. Checks the password for complexity (number input characters.)\n
+    Функция добавляет данные о пользователях в БД. Проверяет пароль на сложность (количество вводимых символов.)
     """
     surname = ent_surname_user.get()
     username = ent_name_user.get()
     passwd = ent_password_user.get()
-    cur.execute('''INSERT INTO users(surname, username, password) 
-                   VALUES (?, ?, ?)''', 
-                   (surname, username, passwd))
-    if len(passwd) < 8:
-        mb.showinfo("Сохранение", f"Данные не сохранены {username}!\nДлина пароля должна\
-            быть не менее 8 символов.") 
-        return False
+
+    if len(passwd) < 5:
+        mb.showinfo("Сохранение", f"Данные не сохранены {username}!\nДлина пароля должна быть не менее 5 символов.")
+        return  False
     else:
         mb.showinfo("Сохранение", f"Данные записаны в базу данных {username}!")
+        cur.execute('''INSERT INTO users(surname, username, password) 
+                    VALUES (?, ?, ?)''', 
+                    (surname, username, passwd))
+        cur.execute('''UPDATE users SET surname=?, username=?, password=? WHERE id=?''', (surname, username, passwd, '#1'))
     conn.commit()
+    conn.close()
     root.destroy()                 
             
 
@@ -46,48 +46,43 @@ root.iconphoto(True, tk.PhotoImage(file="logo.png"))
 
 lbl_new_user = ttk.Label(root, text="Регистрация нового работника")
 lbl_new_user.grid(row=0, column=0, columnspan=2, padx=55, pady=15)
-lbl_new_user.config(font=("Times New Roman", 18, "bold"), foreground="red")
+lbl_new_user.config(font=("Times New Roman", 18), foreground="red")
 
-def validate(input):
-    """
-    The function checks the Entry field for characters entered by the user from the keyboard,\
-        letters must be entered.\n
-    Функция проверяет поле Entry на вводимые символы пользователем с клавиатуры,\
-        должны быть введены буквы.
-    """
-    return input.isalpha()
-valid = root.register(validate)
+# def validate(input):
+#     """
+#     The function checks the Entry field for characters entered by the user from the keyboard, letters must be entered.\n
+#     Функция проверяет поле Entry на вводимые символы пользователем с клавиатуры, должны быть введены буквы.
+#     """
+#     return input.isalpha()
+# valid = root.register(validate)
 
 lbl_surname_user = ttk.Label(root, text="Фамилия")
 lbl_surname_user.grid(row=1, column=0, padx=15, pady=10, sticky=tk.W)
-lbl_surname_user.config(font=("Times New Roman", 12, "bold"))
-ent_surname_user = ttk.Entry(root, validate="key",validatecommand=(valid,"%S"))
+lbl_surname_user.config(font=("Times New Roman", 12))
+ent_surname_user = ttk.Entry(root)#, validate="key",validatecommand=(valid,"%S")
 ent_surname_user.grid(row=1, column=1, padx=15, pady=10)
 ent_surname_user.config(font=("Times New Roman", 12), foreground="grey", width=30)
 
 lbl_name_user = ttk.Label(root, text="Имя")
 lbl_name_user.grid(row=2, column=0, padx=15, pady=10, sticky=tk.W)
-lbl_name_user.config(font=("Times New Roman", 12, "bold"))
-ent_name_user = ttk.Entry(root, validate="key",validatecommand=(valid,"%S"))
+lbl_name_user.config(font=("Times New Roman", 12))
+ent_name_user = ttk.Entry(root)#, validate="key",validatecommand=(valid,"%S")
 ent_name_user.grid(row=2, column=1, padx=15, pady=10)
 ent_name_user.config(font=("Times New Roman", 12), foreground="grey", width=30)
 
 lbl_password_user = ttk.Label(root, text="Пароль*")
 lbl_password_user.grid(row=3, column=0, padx=15, pady=10, sticky=tk.W)
-lbl_password_user.config(font=("Times New Roman", 12, "bold"))
+lbl_password_user.config(font=("Times New Roman", 12))
 ent_password_user = ttk.Entry(root, show="*")
 ent_password_user.grid(row=3, column=1, padx=15, pady=10)
 ent_password_user.config(font=("Times New Roman", 12), foreground="#000000", width=30)
 
-lbl_new_user = ttk.Label(root, text="* Пароль должен состоять из строчных и заглавных букв,\
-цифр и символов")
-lbl_new_user.grid(row=4, column=0, columnspan=2, padx=35, pady=5, sticky=tk.E)
-lbl_new_user.config(font=("Times New Roman", 8, "bold"))
+lbl_new_user = ttk.Label(root, text="* Пароль должен состоять из строчных и заглавных букв, цифр и символов", foreground="blue")
+lbl_new_user.grid(row=4, column=0, columnspan=2, padx=35, pady=5, sticky=tk.W)
+lbl_new_user.config(font=("Times New Roman", 9, "italic"))
 
 btn_submit_db = ttk.Button(root, text="Сохранить", width=25, command=save_users)
 btn_submit_db.grid(row=5, column=1, padx=35, pady=20, sticky=tk.NE)
 
-# if __name__ == "__main__":
-#     main()
 
 root.mainloop()
